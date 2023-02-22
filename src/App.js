731c76ae0a862ef7f58ce64e025ed01ab6aa30ea
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-02-23 04:56:37 +0530

    Donw till video 7

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 
 import './App.css';
+import Navbar from './component/Navbar';
 
 function App() {
-  let shr = "Shreyash";
   return (
     <>
-    <h1>Hello {shr}</h1>
-    <nav>
-     <li>Home</li>
-     <li>Yellow</li>
-     <li>Park</li>
-     </nav>
-
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse cupiditate quasi aliquid laborum consequatur blanditiis dolore dolorum cumque quibusdam perferendis, amet, eius iure ad facere aliquam porro? Accusamus quae veniam odio perspiciatis.
+    <Navbar title = "TextUtiiiiiii" aboutText = " Yo I am Text Utils"/>
     </>
   );
 }
